f79ea0c62d6bddf1a173241bb09fa8f9277ec2d2
James Edens <jedensuscg@gmail.com>
2020-05-14 00:28:11 -0400

    Added prevetion of duplicate guess logic. Added autowin if no valid guesses left. added quit button. Added History and number of guesses.

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NumberWizard : MonoBehaviour
 {
 
-    [SerializeField] int max;
+    int max;
     [SerializeField] int min;
+    int numberOfGuesses;
     int guess;
-    //List<int> previousGuess = new List<int>();
+    bool showHistory = false;
+    string guessHistory;
+    [SerializeField] TextMeshProUGUI guessCountText;
+    [SerializeField] Text guessHistoryText;
+    List<int> previousGuess = new List<int>();
     [SerializeField] TextMeshProUGUI guessText;
+    [SerializeField] Text guessHeader;
+    [SerializeField] Text guessSubHeader;
+    [SerializeField]
+
 
     // Use this for initialization
     void Start()
@@ -20,30 +30,79 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        max = max + 1;
-        guess = Random.Range(max, min);
+        max = StartLogic.newMax;
+        guessHistoryText.enabled = false;
+        max = max;
+        guess = Random.Range(min, max + 1);
         guessText.text = guess.ToString();
+        previousGuess.Add(guess);
+        numberOfGuesses += 1;
+        guessHistory = (string.Join(System.Environment.NewLine, previousGuess));
+        guessHistoryText.text = guessHistory.ToString();
     }
 
     void NextGuess()
     {
-        max = max + 1;
-        guess = Random.Range(max, min);
+        guess = Random.Range(min, max + 1);
+        while (true)
+        {
+            if(previousGuess.Contains(guess))
+            {
+                if(max-min <= 1)
+                {
+                    guessHeader.fontSize = 32;
+                    guessHeader.text =  $"YOUR NUMBER MUST BE {guess}, OR YOU FORGOT YOUR NUMBER!";
+                    guessSubHeader.text = "BECAUSE I AM OUT OF NUMBERS TO GUESS.";
+                    
+                    break;
+                }
+                else
+                {
+                    guess = Random.Range(min, max + 1);
+                    Debug.Log("DUPLICATE ANSWER");
+                }
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        Debug.Log(max - min);
+        numberOfGuesses += 1;
         guessText.text = guess.ToString();
-        //previousGuess.Add(guess);
+        guessCountText.text = numberOfGuesses.ToString();
+        previousGuess.Add(guess);
+
+        guessHistory = (string.Join(System.Environment.NewLine, previousGuess));
+        guessHistoryText.text = guessHistory.ToString();
 
     }
 
     public void OnPressHigher()
     {
-        max = guess;
+        min = guess;
         NextGuess();
     }
 
     public void OnPressLower()
     {
-        min = guess;
+        max = guess;
         NextGuess();
     }
 
+    public void displayHistory()
+    {
+        showHistory = !showHistory;
+        if(showHistory)
+        {
+            guessHistoryText.enabled = true;
+        }
+        else
+        {
+            guessHistoryText.enabled = false;
+        }
+    }
+
+    
 }
---
+++ Assets/Scripts/StartLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartLogic : MonoBehaviour
+{
+    public static int newMax;
+    [SerializeField] Text maxNumber;
+    float max = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newMax = 1000;
+        maxNumber.text = max.ToString();
+    }
+
+    public void OnvalueChanged(float newValue)
+    {
+        max = newValue;
+        maxNumber.text = max.ToString();
+        newMax = (int)max;
+    }
+}
