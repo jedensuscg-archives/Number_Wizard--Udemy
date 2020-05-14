d0bb3fdbcc9fb5c82094fa37851613989692b059
James Edens <jedensuscg@gmail.com>
2020-05-13 21:41:09 -0400

    Finished main logic, Can play game through.

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    //List<int> previousGuess = new List<int>();
+    [SerializeField] TextMeshProUGUI guessText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+        guess = Random.Range(max, min);
+        guessText.text = guess.ToString();
+    }
+
+    void NextGuess()
+    {
+        max = max + 1;
+        guess = Random.Range(max, min);
+        guessText.text = guess.ToString();
+        //previousGuess.Add(guess);
+
+    }
+
+    public void OnPressHigher()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+}
